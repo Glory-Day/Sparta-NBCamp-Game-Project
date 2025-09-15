c45556aa16da76a134b24306b680ce926bf0eefa
Glory Day <alwaysgloryday@gmail.com>
2025-09-15 20:22:12 +0900

    Fix: Addressable 주소 이름 출력 오류 및 파일 생성 기능 추가
    - 주소 이름이 출력 안되는 문제 해결
    - Fetch 버튼 제거

--- Project/Assets/Script/Util/Editor/Addressables/AdressAssetSettingWindow.cs
+++ Project/Assets/Script/Util/Editor/Addressables/AdressAssetSettingWindow.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 using UnityEngine;
 using Backend.Util.Debug;
 
-namespace Backend.Util.Editor.Addressable
+namespace Backend.Util.Editor.Addressables
 {
     public class AddressAssetSettingWindow : EditorWindow
     {
@@ -25,28 +25,25 @@ namespace Backend.Util.Editor.Addressable
 
         private AddressAssetWindow _parent;
 
-        private readonly string[] _texts = new string[3];
-
         public void OnGUI()
         {
             var lineHeight = EditorGUIUtility.singleLineHeight;
-            var fontSize = EditorStyles.label.fontSize;
 
             var style = new GUIStyle(GUI.skin.label);
             style.fontStyle = FontStyle.Bold;
             style.fontSize = EditorStyles.label.fontSize;
             style.normal.textColor = EditorStyles.label.normal.textColor;
 
-            GUI.Label(new Rect(10f, 10f + lineHeight * 0f + 2f * 0f, 80f, lineHeight), SubjectLabel, style);
-            GUI.Label(new Rect(10f, 10f + lineHeight * 1f + 2f * 1f, 80f, lineHeight), PathLabel);
-            GUI.Label(new Rect(10f, 10f + lineHeight * 2f + 2f * 2f, 80f, lineHeight), NamespaceLabel);
-            GUI.Label(new Rect(10f, 10f + lineHeight * 3f + 2f * 3f, 80f, lineHeight), ClassLabel);
+            GUI.Label(new Rect(10f, 10f + (lineHeight * 0f) + (2f * 0f), 80f, lineHeight), SubjectLabel, style);
+            GUI.Label(new Rect(10f, 10f + (lineHeight * 1f) + (2f * 1f), 80f, lineHeight), PathLabel);
+            GUI.Label(new Rect(10f, 10f + (lineHeight * 2f) + (2f * 2f), 80f, lineHeight), NamespaceLabel);
+            GUI.Label(new Rect(10f, 10f + (lineHeight * 3f) + (2f * 3f), 80f, lineHeight), ClassLabel);
 
-            _parent.Data.Path = GUI.TextField(new Rect(90f, 10f + lineHeight * 1f + 2f * 1f, 180f - lineHeight - 2f, lineHeight), _parent.Data.Path);
-            _parent.Data.Namespace = GUI.TextField(new Rect(90f, 10f + lineHeight * 2f + 2f * 2f, 180f, lineHeight), _parent.Data.Namespace);
-            _parent.Data.ClassName = GUI.TextField(new Rect(90f, 10f + lineHeight * 3f + 2f * 3f, 180f, lineHeight), _parent.Data.ClassName);
+            _parent.Loader.Cache.Path = GUI.TextField(new Rect(90f, 10f + (lineHeight * 1f) + (2f * 1f), 180f - lineHeight - 2f, lineHeight), _parent.Loader.Cache.Path);
+            _parent.Loader.Cache.Namespace = GUI.TextField(new Rect(90f, 10f + (lineHeight * 2f) + (2f * 2f), 180f, lineHeight), _parent.Loader.Cache.Namespace);
+            _parent.Loader.Cache.ClassName = GUI.TextField(new Rect(90f, 10f + (lineHeight * 3f) + (2f * 3f), 180f, lineHeight), _parent.Loader.Cache.ClassName);
 
-            if (GUI.Button(new Rect(270f - lineHeight, 10f + lineHeight * 1f + 2f * 1f, lineHeight, lineHeight), "..."))
+            if (GUI.Button(new Rect(270f - lineHeight, 10f + (lineHeight * 1f) + (2f * 1f), lineHeight, lineHeight), "..."))
             {
                 var path = EditorUtility.OpenFolderPanel("Select Folder Path", Application.dataPath, string.Empty);
                 if (string.IsNullOrEmpty(path) == false && path.StartsWith(Application.dataPath))
@@ -55,15 +52,15 @@ namespace Backend.Util.Editor.Addressable
                     path = path.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
                     path = path.Replace("\\", "/");
 
-                    _parent.Data.Path = path;
+                    _parent.Loader.Cache.Path = path;
                 }
             }
 
             style = new GUIStyle(GUI.skin.button);
             style.alignment = TextAnchor.MiddleCenter;
-            if (GUI.Button(new Rect(230f, 10f + lineHeight * 4f + 2f * 4f, 40f, lineHeight), "Save", style))
+            if (GUI.Button(new Rect(230f, 10f + (lineHeight * 4f) + (2f * 4f), 40f, lineHeight), "Save", style))
             {
-                _parent.SaveAddressableAssetData();
+                _parent.Loader.SaveAddressableAssetCacheData();
             }
         }
 
--- Project/Assets/Script/Util/Json/Data/Addressables/AddressAssetData.cs
+++ Project/Assets/Script/Util/Json/Data/Addressables/AddressAssetData.cs
@@ -1,26 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Backend.Util.Json.Data.Addressables
 {
     [System.Serializable]
     public class AddressAssetData
     {
-        public List<string> Names { get; set; } = new();
+        public string ToConstantFieldName(int index)
+        {
+            return ToConstantFieldName(Addresses[index]);
+        }
+
+        private string ToConstantFieldName(string address)
+        {
+            var extension = Path.GetExtension(address);
+            if (string.IsNullOrEmpty(extension) == false)
+            {
+                extension = char.ToUpper(extension[1]) + extension.Substring(2);
+            }
+
+            var name = Path.ChangeExtension(address, null);
+            name = Regex.Replace(name, @"[^\w]", "_");
+            name = Regex.Replace(name, @"_+", "_");
+            name = name.Trim('_');
+
+            if (char.IsDigit(name[0]))
+            {
+                name = "_" + name;
+            }
+
+            return name + "_" + extension;
+        }
 
         public List<string> Addresses { get; set; } = new();
-
-        public Dictionary<string, List<string>> Groups { get; set; } = new();
-    }
-
-    [System.Serializable]
-    public class AddressAssetCacheData
-    {
-        public string Path { get; set; } = string.Empty;
-
-        public string Namespace { get; set; } = string.Empty;
-
-        public string ClassName { get; set; } = string.Empty;
-
-        public AddressAssetData AddressAssetData { get; set; } = new();
     }
 }
