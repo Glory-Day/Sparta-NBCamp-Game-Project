6e53cae5d4f205110ff069dbf924919bfd99f35e
Glory Day <alwaysgloryday@gmail.com>
2025-09-16 21:26:21 +0900

    Feature: 캐릭터 관련 기본 기능 추가
    - 캐릭터 기본 애니메이션 제어 기능
    - 캐릭터 기본 이동 제어 기능
    - 캐릭터 기본 스테이터스 기능
    - 기본 행동 트리 관련 기능
    - 기본 행동과 스테이터스 관련 데이터

---
+++ Project/Assets/Script/Util/CoolDownTimer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Backend.Util.Debug;
+
+namespace Backend.Util
+{
+    public class CoolDownTimer
+    {
+        private CancellationTokenSource _source;
+
+        private readonly TimeSpan _time;
+
+        public CoolDownTimer(float time)
+        {
+            _time = TimeSpan.FromSeconds(time);
+        }
+
+        public void Start()
+        {
+            if (IsCoolingDown)
+            {
+                return;
+            }
+
+            IsCoolingDown = true;
+
+            _source = new CancellationTokenSource();
+
+            Task.Run(
+                async () =>
+                {
+                    try
+                    {
+                        await Task.Delay(_time, _source.Token);
+                    }
+                    catch (TaskCanceledException exception)
+                    {
+                        Debugger.LogError(exception.Message);
+                    }
+                });
+
+            IsCoolingDown = false;
+        }
+
+        public void Stop()
+        {
+            if (_source != null && _source.IsCancellationRequested)
+            {
+                _source.Cancel();
+
+                IsCoolingDown = false;
+            }
+        }
+
+        public bool IsCoolingDown { get; private set; }
+    }
+}
---
+++ Project/Assets/Script/Util/Data/ActionData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Backend.Util.Data
+{
+    public class ActionData : ScriptableObject
+    {
+        [field: Header("Default Settings")]
+        [field: SerializeField] public string ID { get; private set; }
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public float Damage { get; private set; }
+
+        [field: Header("Audio Settings")]
+        [field: SerializeField] public AudioClip ActionSound { get; private set; }
+    }
+}
