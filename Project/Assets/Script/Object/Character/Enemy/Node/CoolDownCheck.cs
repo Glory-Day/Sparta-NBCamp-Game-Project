fecce97aca79dbd74745efc802cdd16b1b51e699
rootinge <jhpark5129@gmail.com>
2025-09-25 20:53:52 +0900

    Feature: 구미호 비스트 AI 추가 (WIP)
    - 쿨다운 공통 노드 수정
    
    - 선회 움직임
    - 앞으로 움직임
    - 측면 움직임

--- Project/Assets/Script/Object/Character/Enemy/Node/CoolDownCheck.cs
+++ Project/Assets/Script/Object/Character/Enemy/Node/CoolDownCheck.cs
@@ -1,21 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Backend.Object.Character.Enemy.Node
+﻿namespace Backend.Object.Character.Enemy.Node
 {
     public class CoolDownCheck : ActionNode
     {
+        public string ActionID;
+
+        // 쿨다운이 아닐때 쿨다운 미리 시작할지 여부
+        public bool IsPreStartCooldown = false;
+
         protected override State OnUpdate()
         {
             if (agent.CombatController.ActionData != null)
             {
-                if (agent.CombatController.ActionCoolTimer[agent.CombatController.ActionData.ID].IsCoolingDown)
+                if (agent.CombatController.ActionCoolTimer[ActionID].IsCoolingDown)
                 {
                     return State.Failure;
                 }
                 else
                 {
+                    if (IsPreStartCooldown)
+                    {
+                        agent.CombatController.ActionCoolTimer[ActionID].Start();
+                    }
                     return State.Success;
                 }
             }
