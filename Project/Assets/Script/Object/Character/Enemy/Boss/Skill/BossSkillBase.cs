2f181e820db1f43e1793ff711e2d9b186ed30bda
rootinge <jhpark5129@gmail.com>
2025-09-29 19:32:04 +0900

    Feature: 구미호 비스트 AI 추가 (WIP)
    - 스킬 사용 시 이동 추가
    - 스킬 이펙트 추가
    - 움직임 BT 수정

--- Project/Assets/Script/Object/Character/Enemy/Boss/Skill/BossSkillBase.cs
+++ Project/Assets/Script/Object/Character/Enemy/Boss/Skill/BossSkillBase.cs
@@ -13,33 +13,5 @@ namespace Backend.Object.Character.Enemy.Boss.Skill
 
         [field: SerializeField] protected GameObject projectilePrefab;
         [field: SerializeField] protected Transform[] projectileTransform;
-
-        public event Action OnSkillEnd;
-        public IEnumerator ExecuteSkill(EnemyAnimationController animController, ActionBossData data)
-        {
-            animController.SetAnimationTrigger(SkillData.ID);
-            yield return new WaitForSeconds(0.01f);
-
-            yield return StartCoroutine(ExecuteSkillLogic(animController, data));
-
-            TriggerOnSkillEnd();
-            yield return null;
-        }
-
-        // 실제 스킬 로직 구현
-        protected abstract IEnumerator ExecuteSkillLogic(EnemyAnimationController animController, ActionBossData data);
-
-        // 스킬 종료 이벤트 트리거
-        protected void TriggerOnSkillEnd()
-        {
-            Debugger.LogMessage($"{SkillData.name} is SkillEnd");
-            OnSkillEnd?.Invoke();
-            OnSkillEnd = null;
-        }
-
-        private void OnDestroy()
-        {
-            OnSkillEnd = null;
-        }
     }
 }
