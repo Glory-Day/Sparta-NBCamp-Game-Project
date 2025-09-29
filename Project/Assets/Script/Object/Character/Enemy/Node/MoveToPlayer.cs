a256c95c4c6abc4c09e5ab160721524293dce70e
aq0011 <aq0011223344@gmail.com>
2025-09-29 19:36:01 +0900

    Feature: 구미호 인간형 AI 추가 (WIP)
    BT Node
    - 백스텝
    - 애니메이션 스피드 조절
    - 공격 애니메이션
    - 여우불 투사체

--- Project/Assets/Script/Object/Character/Enemy/Node/MoveToPlayer.cs
+++ Project/Assets/Script/Object/Character/Enemy/Node/MoveToPlayer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Backend.Util.Debug;
+using UnityEngine;
 using UnityEngine.AI;
 
 namespace Backend.Object.Character.Enemy.Node
@@ -9,8 +10,12 @@ namespace Backend.Object.Character.Enemy.Node
         public float StopDistance = 2.0f;
         public float Offset = 2.0f;
         public bool EnableRotation = true;
+        public bool EnableUseAnim = false;
 
         private Transform playerTransform;
+
+        private readonly int _animRun = Animator.StringToHash("Run");
+        private readonly int _animIdle = Animator.StringToHash("Idle");
 
         protected override void Start()
         {
@@ -19,18 +24,27 @@ namespace Backend.Object.Character.Enemy.Node
 
             if (agent.MovementController.Target != null)
             {
+                if (EnableUseAnim)
+                {
+                    agent.AnimationController.SetCrossFadeInFixedTime(_animRun, 0.1f);
+                }
                 playerTransform = agent.MovementController.Target.transform;
             }
         }
 
         protected override void Stop()
         {
+            if (EnableUseAnim)
+            {
+                agent.AnimationController.SetCrossFadeInFixedTime(_animIdle, 0.1f);
+            }
         }
 
         protected override State OnUpdate()
         {
             if (playerTransform == null)
             {
+                Debugger.LogError("PlayerTransform is NULL");
                 return State.Failure;
             }
 
--- Project/Assets/Script/Object/Character/Enemy/Node/SpawnProjectile.cs
+++ Project/Assets/Script/Object/Character/Enemy/Node/SpawnProjectile.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Backend.Object.Management;
+using Backend.Object.Projectile;
+using Backend.Util.Debug;
 using UnityEngine;
 
 namespace Backend.Object.Character.Enemy.Node
@@ -11,13 +13,24 @@ namespace Backend.Object.Character.Enemy.Node
         public Vector3[] ProjectileSpawnPoint;
 
         public float Delay = 0.2f;
+        public float Speed = 1f;
+        public float Duration = 1f;
+        public float ChasingTime = 0f;
+        public float SpawnDelay = 0f;
+
         private float _timer;
         private int _spawnCount;
-
+        private float _damage;
         protected override void Start()
         {
             _timer = 0f;
             _spawnCount = 0;
+            _damage = agent.Status.BossStatus.Damage * agent.CombatController.ActionData.Damage;
+
+            if (Duration < SpawnDelay || Duration < ChasingTime)
+            {
+                Debugger.LogError($"Need Set Duration Duration > SpawnDelay + ChasingTime, Now : {Duration} > {SpawnDelay} + {ChasingTime}");
+            }
         }
         protected override void Stop() { }
         protected override State OnUpdate()
@@ -27,7 +40,17 @@ namespace Backend.Object.Character.Enemy.Node
             if (_timer >= Delay && _spawnCount < ProjectileSpawnPoint.Length)
             {
                 var spawnPoint = agent.MovementController.transform.TransformPoint(ProjectileSpawnPoint[_spawnCount]);
-                ObjectPoolManager.SpawnPoolObject(Projectile, spawnPoint, Quaternion.identity, agent.MovementController.transform);
+                GameObject spawnObj = ObjectPoolManager.SpawnPoolObject(Projectile, spawnPoint, Quaternion.identity, null);
+
+                if (spawnObj.TryGetComponent<IProjectile>(out var projectileComponent))
+                {
+                    Transform target = agent.MovementController.Target.transform;
+                    projectileComponent.Initialized(target, _damage, Speed, SpawnDelay, ChasingTime, Duration);
+                }
+                else
+                {
+                    Debugger.LogError($"{Projectile} is Not Have IPorjectile");
+                }
 
                 _spawnCount++;
                 _timer = 0f;
