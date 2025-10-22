5816095d0a78561912524f9cc7bf972a0a9c1cd9
rootinge <jhpark5129@gmail.com>
2025-10-22 16:41:10 +0900

    Feature: 노말 몬스터 Tiger 추가 (WIP)
    - Tiger 움직임
    - FieldOfView : 사거리 안 플레이어 체크
    - NavMeshAnimationSynchronizer : NavMesh 가속도에 따른 애니메이션 파라미터 Speed 변경

---
+++ Project/Assets/Script/Object/Character/Enemy/Node/IsNormalEnemyMoveStop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Backend.Object.Character.Enemy.Node
+{
+    public class IsNormalEnemyMoveStop : ActionNode
+    {
+        protected override void Start()
+        {
+        }
+
+        protected override State OnUpdate()
+        {
+
+
+            // pathPending: 경로 계산 중인지 확인, remainingDistance: 남은 거리
+            if (!agent.NavMeshAgent.hasPath || (!agent.NavMeshAgent.pathPending && agent.NavMeshAgent.remainingDistance <= agent.NavMeshAgent.stoppingDistance))
+            {
+                return State.Success;
+            }
+
+            return State.Running;
+        }
+
+        protected override void Stop()
+        {
+            agent.NavMeshAgent.ResetPath();
+        }
+    }
+}
---
+++ Project/Assets/Script/Object/Character/Enemy/Node/RandomIdle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Backend.Object.Character.Enemy.Node
+{
+    public class RandomIdle : ActionNode
+    {
+        public float MinTime = 1f;
+        public float MaxTime = 3f;
+        private float _waitTime;
+        private float _startTime;
+        protected override void Start()
+        {
+            agent.NavMeshAgent.ResetPath();
+            _startTime = Time.time;
+            _waitTime = Random.Range(MinTime, MaxTime);
+        }
+
+        protected override void Stop()
+        {
+
+        }
+
+        protected override State OnUpdate()
+        {
+            if (Time.time - _startTime >= _waitTime)
+            {
+                return State.Success;
+            }
+
+            return State.Running;
+        }
+    }
+}
