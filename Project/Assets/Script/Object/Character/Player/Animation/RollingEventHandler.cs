349981ce7c27f453e6c1605eaf40c2b77857a381
Glory Day <alwaysgloryday@gmail.com>
2025-10-23 06:05:54 +0900

    Fix: 플레이어 애니메이션 및 기능 수정
    - 구르기 선 입력에 대한 시점 조정
    - 애니메이션 이벤트의 동적 할당 기능 추가

---
+++ Project/Assets/Script/Object/Character/Player/Animation/RollingEventHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Backend.Object.Animation;
+using UnityEngine;
+
+namespace Backend.Object.Character.Player.Animation
+{
+    public class RollingEventHandler : StateMachineBehaviour
+    {
+        [Header("Event Settings")]
+        [SerializeField] private List<float> times;
+
+        private AdvancedActionController _controller;
+
+        private readonly AnimationEventRegister _register = new ();
+        private readonly bool[] _triggers = new bool[1];
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            //TODO: 빈도 높은 고자원 메서드 사용 중임으로 차후에 수정해야 한다.
+            _controller = animator.GetComponentInParent<AdvancedActionController>();
+            _register.Register(IsRollButtonBufferedValid);
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var progress = stateInfo.normalizedTime % 1f;
+
+            if (_triggers[0] == false && progress >= times[0])
+            {
+                _triggers[0] = true;
+                _register.Invoke(0);
+            }
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _register.Unregister(IsRollButtonBufferedValid);
+            _controller.IsRollButtonBufferable = false;
+            _triggers[0] = false;
+        }
+
+        private void IsRollButtonBufferedValid()
+        {
+            _controller.IsRollButtonBufferable = true;
+        }
+    }
+}
