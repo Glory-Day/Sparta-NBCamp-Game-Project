36cba67d06766797856ecad1c73d9b8e3d07ebb8
Glory Day <alwaysgloryday@gmail.com>
2025-10-22 19:54:30 +0900

    Fix: 플레이어 이동 관련 수정
    - 전반적인 공격 기능 판정 수정
    - 공격 시점에 이동 및 구르기 기능 바로 적용되게 적용

---
+++ Project/Assets/Script/Object/Animation/AnimationMovementController.cs
@@ -0,0 +1,29 @@
+﻿using Backend.Object.Character.Player;
+using UnityEngine;
+
+namespace Backend.Object.Animation
+{
+    public class AnimationMovementController : StateMachineBehaviour
+    {
+        public AnimationCurve speed;
+
+        private AdvancedActionController _controller;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _controller = animator.GetComponentInParent<AdvancedActionController>();
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var normalized = stateInfo.normalizedTime % 1f;
+
+            _controller.deltaSpeed = speed.Evaluate(normalized);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _controller.deltaSpeed = 1f;
+        }
+    }
+}
