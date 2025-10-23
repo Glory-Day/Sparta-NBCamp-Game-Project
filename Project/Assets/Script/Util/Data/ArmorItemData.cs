9556cfd8ddb204a08a35a4b417c462e149800d03
Glory Day <alwaysgloryday@gmail.com>
2025-10-23 19:58:59 +0900

    Feature: 플레이어 관련 아이템 데이터 기능 추가
    - 기본 아이템 데이터 기능
    - 소모용 아이템 데이터 기능
    - 무기 아이템 데이터 기능
    - 방어구 아이템 데이터 기능

---
+++ Project/Assets/Script/Util/Data/ArmorItemData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Backend.Util.Data
+{
+    [CreateAssetMenu(fileName = "Armor_Item_Data", menuName = "Scriptable Object/Data/Armor Item Data")]
+    public class ArmorItemData : EquipableItemData
+    {
+        [field: Header("Defense Point Settings")]
+        [field: SerializeField] public float PhysicalDefensePoint { get; set; }
+        [field: SerializeField] public float MagicalDefensePoint { get; set; }
+    }
+}
