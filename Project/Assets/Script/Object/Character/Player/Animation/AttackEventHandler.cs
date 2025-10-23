163d387cac7e5eb1c25f14c722ae83c641c445c7
Glory Day <alwaysgloryday@gmail.com>
2025-10-23 19:57:53 +0900

    Fix: 플레이어 애니메이션 기능 수정
    - 플레이어 애니메이션 이동 기능 수정
    - 각 공격 애니메이션 별 속도 그래프 조정

--- Project/Assets/Script/Object/Character/Player/Animation/AttackEventHandler.cs
+++ Project/Assets/Script/Object/Character/Player/Animation/AttackEventHandler.cs
@@ -11,35 +11,35 @@ namespace Backend.Object.Character.Player.Animation
 
         private DamageSender _sender;
 
-        private readonly AnimationEventRegister _register = new ();
+        private readonly AnimationEventRegistry _registry = new ();
         private readonly bool[] _triggers = new bool[2];
 
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             //TODO: 빈도 높은 고자원 메서드 사용 중임으로 차후에 수정해야 한다.
             _sender = animator.GetComponentInChildren<DamageSender>();
-            _register.Register(_sender.StartDetection);
-            _register.Register(_sender.StopDetection);
+            _registry.Register(_sender.StartDetection);
+            _registry.Register(_sender.StopDetection);
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             var progress = stateInfo.normalizedTime % 1f;
 
-            for (var i = 0; i < _register.Length; i++)
+            for (var i = 0; i < _registry.Length; i++)
             {
                 if (_triggers[i] == false && progress >= times[i])
                 {
                     _triggers[i] = true;
-                    _register.Invoke(i);
+                    _registry.Invoke(i);
                 }
             }
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            _register.Unregister(_sender.StartDetection);
-            _register.Unregister(_sender.StopDetection);
+            _registry.Unregister(_sender.StartDetection);
+            _registry.Unregister(_sender.StopDetection);
             _triggers[0] = false;
             _triggers[1] = false;
         }
